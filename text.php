f1309f6a10859917a2dcfd370eb403c648b15d79
Siael Alves <siaelalves@outlook.com>
2025-01-03 11:04:18 -0300

    FEAT Adicionado arquivo de inicialização para a namespace `text`.

---
+++ text.php
@@ -0,0 +1,10 @@
+<?php
+/* © Copyright 2024, Siael Alves */
+
+/* Conjunto de classes e métodos usadas para manipular texto. */
+namespace text ;
+
+/**
+ * Classe responsável por agrupar funções para escrita de texto. 
+ */
+require "text/writing.php" ;
