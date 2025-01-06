4994f80d5f4c785b771a2dbebcb4bed6fc8b169f
Siael Alves <siaelalves@outlook.com>
2025-01-06 11:31:32 -0300

    REFACTOR Melhorados comentários das funções.
    Adicionados 2 novas propriedades: `chars` e `words`.
    
    Função `remove_punctuation( )` renomeada para `replace_punctuation( )`.

--- writing.php
+++ writing.php
@@ -12,6 +12,12 @@ class writing {
  /** @var string $string String ou texto que se pretende manipular. */
  public string $string ;
 
+ /** @var int $chars Número de caracteres na string. */
+ public int $chars ;
+
+ /** @var int $words Número de palavras na string. */
+ public int $words ;
+
 
 
  /**
@@ -22,21 +28,28 @@ class writing {
 
   $this->string = $string ;
 
+  $this->chars = strlen ( $string ) ;
+
+  $this->words = str_word_count ( $string ) ;
+
  }
 
  /**
-  * Corrige a acentuação em letras maiúsculas,
+  * Corrige a acentuação durante a codificação do texto para o navegador,
   * impedindo, por exemplo, que "RÓTULOS" apareça como "RóTULOS", por exemplo.
-  * @param string $step Define se irá corrigir as letras maiúsculas ou minúsculas.
+  * @param string $string Define a string cuja acentuação será corrigida. Se for vazio, 
+  * a string definida no construtor será usada.
+  * @param bool $all_caps Se for true, retornará a string com todas as letras maiúsculas. 
+  * Se for false, retornará a string com todas as letras minúsculas.
   * @return string Retorna o texto corrigido com a acentuação correta. Se houver um 
-  * argumento incorreto, retornará uma string  vazia;
+  * argumento incorreto, retornará uma string  vazia.
   **/
- public function correct_font ( string $step , bool $all_caps ) : string {
+ public function correct_font ( string $string = "" , bool $all_caps = false ) : string {
   
   $small_letters = array ( "á","é","í","ó","ú","ç","â","ê","ô","à","ã","õ" ) ;
   $big_letters = array ( "Á","É","Í","Ó","Ú","Ç","Â","Ê","Ô","À","Ã","Õ" ) ;
   
-  if ( $step == "" ) {
+  if ( $string == "" ) {
 
    if ( $all_caps == true ) {
     return str_replace ( $small_letters , $big_letters,  $this->string );	
@@ -49,10 +62,10 @@ class writing {
   } else {
 
    if ( $all_caps == true ) {
-    return str_replace ( $small_letters , $big_letters,  $step );	
+    return str_replace ( $small_letters , $big_letters,  $string );	
 
    } else {
-    return str_replace ( $big_letters , $small_letters , $step );
+    return str_replace ( $big_letters , $small_letters , $string );
 
    }
 
@@ -60,57 +73,70 @@ class writing {
 
  }
 
- /** Remove a acentuação de uma string. Funciona apenas com o idioma pt-br.
+ /** 
+  * Remove a acentuação de uma string. Funciona apenas com o idioma pt-br.
+  * @param string $string String cuja acentuação será removida. Se for vazio, 
+  * a string definida no construtor será usada.
   * @return string Retorna uma string com o texto sem acentos.
  */
- public function remove_accents ( string $step = "" ) : string {
+ public function remove_accents ( string $string = "" ) : string {
   
   $with_accents = array ( "á","é","í","ó","ú","â","ê","ô","à","ã","õ","ç","Á","É","Í","Ó","Ú","Â","Ê","Ô","À","Ã","Õ","Ç" ) ;
   $no_accents   = array ( "a","e","i","o","u","a","e","o","a","a","o","c","A","E","I","O","U","A","E","O","A","A","O","C" ) ;
 
-  if ( $step == "" ) {
+  if ( $string == "" ) {
    
    return str_replace ( $with_accents , $no_accents , $this->string ) ;
 
   } else {
 
-   return str_replace ( $with_accents , $no_accents , $step ) ;
+   return str_replace ( $with_accents , $no_accents , $string ) ;
 
   }
   
 
  }
 
- /** Remove a pontuanção de uma string ou a substitui por um símbolo específico. Funciona apenas com o idioma pt-br.
-  * @param string $replaceWith Expressão ou caractere a inserir no lugar de cada caractere de pontuanção.
+ /** 
+  * Remove a pontuanção de uma string ou a substitui por um símbolo específico. Funciona apenas com o idioma pt-br.
+  * @param string $string String cuja pontuação será removida. Se for vazio,
+  * a string definida no construtor será usada.
+  * @param string $replaceWith Expressão ou caractere a inserir no lugar de cada caractere de pontuanção. 
+  * Se não especificado, será substituído por "" (vazio).
   * @return string Retorna uma string com o texto sem pontuação.
  */
- public function remove_punctuation ( string $step = "" , string $replaceWith = "" ) : string {
+ public function replace_punctuation ( string $string = "" , string $replaceWith = "" ) : string {
 
   $signals = array ( ".","!","?",",",";",":","“","”",'"',"'","(",")","[","]","{","}","=","*","&","%","$","#","@","+","/","\\","|",">","<" ) ;
 
-  if ( $step == "" ) {
+  if ( $string == "" ) {
 
    return str_replace ( $signals , $replaceWith , $this->string ) ;
 
   } else {
    
-   return str_replace ( $signals , $replaceWith , $step ) ;
+   return str_replace ( $signals , $replaceWith , $string ) ;
 
   }
 
  }
 
  /** Gera uma string válida para ser usada como Url de uma página, ou como identificador legível de elementos web.
-  * @param string $separator Separador dos símbolos a serem substituídos. 
-  * Ex.: Se $string="eu sou miguel", $separator será usado no lugar de todos os espaços, resultando em: "eu_sou_miguel".
-  * @return string Retorna uma string com uma string válida para se tornar um identificador legível.
+  * @param string $separator Separador das palavras a ser utilizado. O padrão é "-" (hífen).
+  * @example # Exemplo de uso: 
+  * ```
+  * $my_string = new \text\writing("eu sou miguel");
+  * echo $my_string->to_url("_");
+  * // Retornará:
+  * // eu_sou_miguel
+  * ```
+  * @return string Retorna uma string com uma string válida para ser usada como uma URL válida.
  */
  public function to_url ( string $separator = "-" ) : string {
   
   $step1 = strtolower ( str_replace ( ' ' , $separator , $this->string ) ) ;
   
-  $step2 = $this->remove_punctuation ( $step1 , $separator ) ;
+  $step2 = $this->replace_punctuation ( $step1 , $separator ) ;
 
   $step3 = $this->correct_font ( $step2, false );
 
